875c7934266ba60bff9f76067e3f2dfcad73ac80
ajnobre <alvarojnobre@gmail.com>
2019-06-28 11:12:30 +0100

    Explore page implemented v1

--- lib/pages/explore.dart
+++ lib/pages/explore.dart
@@ -13,25 +13,85 @@ class ExplorePage extends StatefulWidget {
 class _ExplorePageState extends State<ExplorePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Explore"),
-          backgroundColor: Colors.green,
-        ),
-        body: MainContent(),
-        drawer: NavigationDrawer());
+    return Scaffold(body: ExploreContent());
   }
 }
 
-class MainContent extends StatelessWidget {
+class ExploreContent extends StatefulWidget {
+  @override
+  _ExploreContentState createState() => _ExploreContentState();
+}
+
+class _ExploreContentState extends State<ExploreContent> {
   Dio dio = new Dio();
   Response response;
-  List<dynamic> list;
-  Future getAllTrails() async {
+  final _suggestions = <Text>[];
+  final _contents = <Map<String, dynamic>>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  List<dynamic> receivedList;
+
+  @override
+  Widget build(BuildContext context) {
+    var futureBuilder = new FutureBuilder(
+      future: _getData(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Center(child: new CircularProgressIndicator());
+          default:
+            if (snapshot.hasError)
+              return new Text('Error: ${snapshot.error}');
+            else
+              return createListView(context, snapshot);
+        }
+      },
+    );
+    return new Scaffold(
+      body: futureBuilder,
+      drawer: NavigationDrawer(),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Explore'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRow(Map<String, dynamic> trail) {
+    return FlatButton(
+      child: ListTile(
+        title: Text(
+          trail['Name'],
+          style: _biggerFont,
+        ),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget buildText(String s) {
+    return Text(s);
+  }
+
+  Future<dynamic> _getData() async {
+    List<dynamic> values;
+    values = await _submit(0);
+    values.addAll(await _submit(1));
+    return values;
+  }
+
+  Future getAllTrails(int offset) async {
     String url =
         'https://atypicaltrailsweb.appspot.com/rest/trails/getAllTrails';
+
     try {
-      response = await dio.post(url, data: {"offset": 0});
+      response = await dio.post(url, data: {"offset": offset});
     } on DioError catch (e) {
       response = e.response;
     }
@@ -39,37 +99,33 @@ class MainContent extends StatelessWidget {
     return response;
   }
 
-  Future _submit() async {
-    response = await getAllTrails();
+  Future _submit(int offset) async {
+    response = await getAllTrails(offset);
     if (response.statusCode == 200) {
-      list = response.data;
-      print(list[1]['key']);
+      receivedList = response.data;
+      return receivedList;
     }
-    print(response.statusCode);
+
   }
 
-  FlatButton buildButtons() {
-    for (int i = 0; i < 12; i++) {
-      return new FlatButton(
-        child: Text("Get Trails"),
-        onPressed: () {
-          _submit();
-        },
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    _submit();
-    return ListView(
+  Widget createListView(BuildContext context, AsyncSnapshot snapshot) {
+    List<dynamic> trailList = snapshot.data;
+    return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      children: <Widget>[
-        FlatButton(
-          child: Text("Get Trails"),
-          onPressed: () {},
-        ),
-      ],
+      itemCount: trailList.length,
+      itemBuilder: (context, i) {
+
+        if (i >= _suggestions.length) {
+          if (i < trailList.length) {
+            _contents.add(trailList[i]['propertyMap']);
+          }
+        }
+        if (i < trailList.length) {
+          return _buildRow(_contents[i]);
+        }
+        return null;
+      },
     );
   }
 }
+
